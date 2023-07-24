71fd7c73a9a1591d39c977c025cac3648c327099
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2023-07-24 23:03:02 +0200

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/purpleturtll/ort-test-1
+
+go 1.20
+
+require github.com/purpleturtll/ort-test-2 v0.0.0-20230724210021-59287c79cabe
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	adder "github.com/purpleturtll/ort-test-2"
+)
+
+func main() {
+	fmt.Println(adder.Add(2, 2))
+}
